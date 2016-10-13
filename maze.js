ee88e424b8e2593e9667fe053d3305ba82244288
Topaz Grant <Topaz Grant>
2016-10-13 12:02:08 -0500

    Single boundary turns red

--- maze.js
+++ maze.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+window.onload = function()
+{
+	
+	var myEl = document.getElementById("boundary1");
+	myEl.addEventListener("mouseover", function(){
+		myEl.setAttribute("class", "boundary youlose");
+	});
+}
+
+
+
+
+
