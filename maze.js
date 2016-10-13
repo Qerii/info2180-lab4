b158def64e16a57b6373743e95973d404a40bb57
Topaz Grant <Topaz Grant>
2016-10-13 18:31:12 -0500

    Exercise2 updated

--- maze.js
+++ maze.js
@@ -1,13 +1,36 @@
 // JavaScript Document
 
-window.onload = function()
+
+//SECTION#1-------------------------------------------------------------
+function leftDiv()
 {
 	
 	var myEl = document.getElementById("boundary1");
-	myEl.addEventListener("mouseover", function(){
-		myEl.setAttribute("class", "boundary youlose");
-	});
+	myEl.addEventListener("mouseover", outOfBound);
 }
+
+function outOfBound(){
+	var myEl = document.getElementById("boundary1");
+	myEl.setAttribute("class", "boundary youlose");
+}
+
+
+//SECTION#2-------------------------------------------------------------
+window.onload = function outBound()
+{
+	var allBoundary = document.querySelectorAll(".boundary");
+	for( var i = 0; i<allBoundary.length; i++){
+		allBoundary[i].addEventListener("mouseover", error);
+	}
+		
+}
+function error () {
+	var allBoundary = document.querySelectorAll(".boundary");
+	for (var x = 0; x<allBoundary.length; x++){
+		allBoundary[x].setAttribute("class", "boundary youlose");
+	}
+}
+
 
 
 
